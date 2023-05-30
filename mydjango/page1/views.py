8b7ca910ec30660d330788c08ce9195d0c1b6abf
hridyansh-gautam <hridyanshg.211cs223@gmail.com>
2023-05-31 01:32:44 +0530

    ok

--- mydjango/page1/views.py
+++ mydjango/page1/views.py
@@ -1,47 +1,46 @@
-from django.shortcuts import redirect, render, HttpResponse
-from django.contrib.auth.models import User
-from django.contrib import messages
+from django.shortcuts import render, HttpResponse
+
 # Create your views here.
+
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
+
 
 def author(request):
-    return render(request,'author.html')
+    return render(request, 'author.html')
+
 
 def issuebooks(request):
-    return render(request,'issuebooks.html')
+    return render(request, 'issuebooks.html')
+
 
 def issuebooks(request):
     if request.method == "POST":
         booktitle = request.POST.get('booktitle')
         authorname = request.POST.get('authorname')
         ISBN = request.POST.get('ISBN')
-    return render(request,'review')
+    return render(request, 'review')
+
 
 def landing(request):
-    return render(request,'landing.html')
+    return render(request, 'landing.html')
+
 
 def publishbook(request):
-    return render(request,'publishbook.html')
+    return render(request, 'publishbook.html')
+
 
 def register(request):
-    if request.method == "POST":
-        username=request.POST['username']
-        password=request.POST['password']
-        myuser = User.objects.create_user(username,password)
-        myuser.save()
-        
-        messages.success(request,"Account created successfully!")
-
-        return redirect('signin')
-
     return render(request,'register.html')
 
+
 def returnbook(request):
-    return render(request,'returnbook.html')
+    return render(request, 'returnbook.html')
 
 
 def review(request):
@@ -51,20 +50,22 @@ def review(request):
         review = request.POST.get('review')
     
     return render(request,'review.html')
+>>>>>>> 29247c1f3427ab3c4cf3390ead4377f4c5657032
 
 
 def signin(request):
-    return render(request,'signin.html')
+    return render(request, 'signin.html')
 
-def userdashboard(request):
-    return render(request,'userdashboard.html')
+# def userdashboard(request):
+#     return render(request,'userdashboard.html')
+
 
 def userdashboard(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email')
-    return render(request,'home')
+    return render(request, 'userdashboard.html')
 
 
 def publishbook(request):
@@ -72,7 +73,7 @@ def publishbook(request):
         booktitle = request.POST.get('booktitle')
         authorname = request.POST.get('authorname')
         ISBN = request.POST.get('ISBN')
-        publdate = request.POST.get('publdate')
         desc = request.POST.get('desc')
-    return render(request,'home')
-
+        publishbook = BookPublish(booktitle=booktitle, authorname=authorname, ISBN=ISBN, publdate=publdate, desc=desc)
+        publishbook.save()
+    return render(request, 'home')
