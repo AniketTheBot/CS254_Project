8adba6490f0dbb1242b4baae1b8df4b9227ddd10
AniketTheBot <aniloona@gmail.com>
2023-05-31 01:19:12 +0530

    a lot changes

--- mydjango/page1/views.py
+++ mydjango/page1/views.py
@@ -1,60 +1,77 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from page1.models import BookReview, BookIssue, BookPublish
 
 # Create your views here.
+
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
+
 
 def author(request):
-    return render(request,'author.html')
+    return render(request, 'author.html')
+
 
 def issuebooks(request):
-    return render(request,'issuebooks.html')
+    return render(request, 'issuebooks.html')
+
 
 def issuebooks(request):
     if request.method == "POST":
         booktitle = request.POST.get('booktitle')
         authorname = request.POST.get('authorname')
         ISBN = request.POST.get('ISBN')
-    return render(request,'review')
+    return render(request, 'review')
+
 
 def landing(request):
-    return render(request,'landing.html')
+    return render(request, 'landing.html')
+
 
 def publishbook(request):
-    return render(request,'publishbook.html')
+    return render(request, 'publishbook.html')
+
 
 def register(request):
-    return render(request,'register.html')
+    return render(request, 'register.html')
+
 
 def returnbook(request):
-    return render(request,'returnbook.html')
+    return render(request, 'returnbook.html')
 
 def review(request):
     return render(request,'review.html')
+
 
 def review(request):
     if request.method == "POST":
         name = request.POST.get('name')
         booktitle = request.POST.get('booktitle')
         review = request.POST.get('review')
-    return render(request,'review')
+        bookreview = BookReview(
+            name=name, booktitle=booktitle, review=review, date=datetime.today())
+        bookreview.save()
+    return render(request, 'review.html')
 
 
 def signin(request):
-    return render(request,'signin.html')
+    return render(request, 'signin.html')
 
-def userdashboard(request):
-    return render(request,'userdashboard.html')
+# def userdashboard(request):
+#     return render(request,'userdashboard.html')
+
 
 def userdashboard(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email')
-    return render(request,'home')
+    return render(request, 'userdashboard.html')
 
 
 def publishbook(request):
@@ -64,5 +81,6 @@ def publishbook(request):
         ISBN = request.POST.get('ISBN')
         publdate = request.POST.get('publdate')
         desc = request.POST.get('desc')
-    return render(request,'home')
-
+        publishbook = BookPublish(booktitle=booktitle, authorname=authorname, ISBN=ISBN, publdate=publdate, desc=desc)
+        publishbook.save()
+    return render(request, 'home')
