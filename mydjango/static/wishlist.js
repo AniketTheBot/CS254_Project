72426946fd466070d2fb380240f1011da1009a60
abrokedrawer37 <noryokusha.37@gmail.com>
2023-05-31 10:27:01 +0530

    l

--- mydjango/static/wishlist.js
+++ mydjango/static/wishlist.js
@@ -1,11 +1,24 @@
 let wishlist = [];
 
 function addToWishlist(imageId) {
+    const iconElement = document.querySelector(`i[data-id="${imageId}"]`);
+
     if (!wishlist.includes(imageId)) {
         wishlist.push(imageId);
         localStorage.setItem('wishlist', JSON.stringify(wishlist));
+        if (iconElement) {
+            iconElement.classList.add('wishlisted');
+        }
+    } else {
+        const index = wishlist.indexOf(imageId);
+        wishlist.splice(index, 1);
+        localStorage.setItem('wishlist', JSON.stringify(wishlist));
+        if (iconElement) {
+            iconElement.classList.remove('wishlisted');
+        }
     }
 }
+
 
 function displayWishlistedImages() {
     const wishlistedContainer = document.getElementById('wishlisted-container');
@@ -16,6 +29,10 @@ function displayWishlistedImages() {
     }
 
     wishlist.forEach(imageId => {
+        const iconElement = document.querySelector(`i[data-id="${imageId}"]`);
+        if (iconElement) {
+            iconElement.classList.add('wishlisted');
+        }
         const imageElement = document.querySelector(`img[data-id="${imageId}"]`);
         if (imageElement) {
             const clonedImage = imageElement.cloneNode(true);
@@ -23,6 +40,7 @@ function displayWishlistedImages() {
         }
     });
 }
+
 
 if (document.getElementById('wishlisted-container')) {
     displayWishlistedImages();
